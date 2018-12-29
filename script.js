f816516d7a1ea0ac6c47f69af03338dc6caa23ec
Andrey Butenko <ghostmancer@gmail.com>
2018-12-29 10:29:50 -0800

    Add comments

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ const config = {
   }
 }
 
+/**
+ * Create an array of a length filled with a value.
+ * @param {*} value Value to put in resulting array.
+ * @param {number} length Length of resulting array.
+ * @returns {Array}
+ */
 function createFilledArray(value, length) {
   const arr = [];
   for(let i = 0; i < length; i++) {
@@ -19,6 +25,13 @@ function createFilledArray(value, length) {
   return arr;
 }
 
+/**
+ * Create an array of a length filled with a computed value.
+ * @param {function} valueFunction Function that returns a value to be put in resulting array.
+ *  Current index is passed as an argument.
+ * @param {number} length Length of resulting array.
+ * @returns {Array}
+ */
 function calculateArray(valueFunction, length) {
   const arr = [];
   for(let i = 0; i < length; i++) {
@@ -28,8 +41,10 @@ function calculateArray(valueFunction, length) {
 }
 
 /**
- * Calculate average amount of duties in a set of persons.
- * @param {Array<Person>} persons 
+ * Calculate average amount of duties in an array of persons.
+ * @param {Array<Person>} persons Array of persons.
+ * @param {number} numDutyTypes Number of types of duty.
+ * @returns {Array<number>} Array of average amount of duties with length numDutyTypes.
  */
 function calculateDutyAverage(persons, numDutyTypes) {
   return persons
@@ -53,15 +68,21 @@ class Person {
     this.targetNumDuties = targetNumDuties; // TODO value should be calculated
   }
 
+  /**
+   * Get the person's name.
+   * @returns {string}
+   */
   getName() {
     return this.name;
   }
 
+  /**
+   * Get the number of duties a person is assigned to for each type.
+   * @returns {Array<number>} Array of numbers, where index is duty type and value is number of assignments.
+   */
   getNumDuties() {
     const res = createFilledArray(0, this.numDutyTypes);
     for(let i = 0; i < this.assignments.length; i++) {
-      //console.log('HLLO')
-      //console.log(`${this.name}: ${this.assignments[i]}@${i}`)
       if(this.assignments[i] != -1) {
         res[this.assignments[i]]++;
       }
@@ -70,6 +91,10 @@ class Person {
     return res;
   }
 
+  /**
+   * Get the number of duties a person is assigned to for a particular type.
+   * @returns {number}
+   */
   getNumDutiesOfType(typeIndex) {
     return this.getNumDuties()[typeIndex];
   }
@@ -86,6 +111,11 @@ class Person {
     return this.assignments;
   }
 
+  /**
+   * Get number of days since the person's last duty assignment.
+   * @param {number} dayIndex Day index from which to count.
+   * @returns {number} Number of days since the person's last duty assignment. Returns 999 if never assigned.
+   */
   getNumDaysSincePreviousDuty(dayIndex) {
     for(let i = dayIndex - 1; i >= 0; i--) {
       if(this.assignments[i] != -1) {
@@ -100,6 +130,16 @@ class Person {
     return calculateArray(typeIndex => this.getDutyScoreOfType(dayIndex, typeIndex), this.numDutyTypes);
   }
 
+  /**
+   * Gets a score of how suitable a person is to be assigned to duty for a particular day.
+   * Considers their availability, days since their last duty, days remaining in the duty set,
+   * target number of duty assignments, and average duty assignments of others.
+   * @param {number} dayIndex Index of the current day.
+   * @param {number} typeIndex Duty type to consider.
+   * @param {Array<number>} averageDutyAssignments Average number of duty assignments for each type
+   *  for others in the duty set.
+   * @returns {number}
+   */
   getDutyScoreOfType(dayIndex, typeIndex, averageDutyAssignments) {
     let score = 100;
 
@@ -128,6 +168,11 @@ class Person {
     return score;
   }
 
+  /**
+   * Assign person to duty type on a particular day.
+   * @param {number} dayIndex Index of the current day.
+   * @param {number} type Duty type to assign.
+   */
   setAssignment(dayIndex, type) {
     this.assignments[dayIndex] = type;
   }
@@ -160,7 +205,7 @@ for(let i = 0; i < 6; i++) {
 
 
 
-class DutySet {
+class DutySet2 {
   /**
    * Constructor for a DutySet.
    * @param {number} numDays Number of days that RAs are on duty in this set.
@@ -228,6 +273,7 @@ class DutySet {
   }
 }
 
+/*
 const avail = {
   Andrey: [true, false, true, false, false],
   Korra: [true, true, false, false, true],
@@ -236,3 +282,4 @@ const avail = {
 
 const weekdays = new DutySet(10, 2, avail);
 //console.log(weekdays)
+*/
